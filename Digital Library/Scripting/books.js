550b6173d3b6bb2cdccf87275b3e28a5bb818199
HamadaCoding <138389818+HamadaCoding@users.noreply.github.com>
2025-04-14 18:00:56 +0200

    Add files via upload

---
+++ Digital Library/Scripting/books.js
@@ -0,0 +1,15 @@
+function home() {
+    window.location = "../../index.html"
+}
+
+function books() {
+    window.location = "../../Html/books.html"
+}
+
+function feedback() {
+    window.location = "../../Html/feedback.html"
+}
+
+function contact() {
+    window.location = "../../Html/contact.html"
+}
---
+++ Digital Library/Scripting/script.js
@@ -0,0 +1,57 @@
+const HOME = document.getElementById("home")
+const BOOKS = document.getElementById("books")
+const FEEDBACK = document.getElementById("feedback")
+const CONTACT_US = document.getElementById("contact")
+const CURRICULUM = document.getElementById("curriculum")
+const HORROR = document.getElementById("horror")
+const ROMANTIC = document.getElementById("romantic")
+const FUNNY = document.getElementById("funny")
+const ADVENTURE = document.getElementById("adventure")
+
+function home() {
+    window.location = "../index.html"
+}
+
+function books() {
+    window.location = "../Html/books.html"
+}
+
+function feedback() {
+    window.location = "../Html/feedback.html"
+}
+
+function contact() {
+    window.location = "../Html/contact.html"
+}
+
+function curriculum() {
+    window.location = "Curriculum/curriculum.html"
+}
+
+function horror() {
+    window.location = "Horror/horror.html"
+}
+
+function romantic() {
+    window.location = "Romantic/romantic.html"
+}
+
+function funny() {
+    window.location = "Funny/funny.html"
+}
+
+function adventure() {
+    window.location = "Adventure/adventure.html"
+}
+
+function mystery() {
+    window.location = "Mystery/mystery.html"
+}
+
+function thriller() {
+    window.location = "Thriller/thriller.html"
+}
+
+function fiction() {
+    window.location = "Fiction/fiction.html"
+}
